b06a812b841ef84c0c1bc94029446fcf05ec7a6b
Alexis <113068662+ImitationWorld@users.noreply.github.com>
2022-10-10 20:10:14 -0400

    Create admin.py
    Admin Panel

---
+++ admin.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import re
+
+
+# Connect MYSQL db to pymysql
+connection = 'mysql+pymysql://root:root@localhost/RealEstate'
+
+app = Flask(__name__)
+
+# Connect to SQLAlchemy
+app.config['SECERET_KEY'] = 'SECRET'
+app.config['SQLALCHEMY_DATABASE_URI'] = connection
+db = SQLAlchemy(app)
+
+# Instantiate admin class
+admin = Admin(app)
+
+# Modelling databases
+
+class Clients(db.Model) :
+    ClientID = db.Column(db.integer, primary_key=True)
+    FirstName = db.Column(db.String(25), unique=False, nullable=False)
+    LastName = db.Column(db.String(25), unique=False, nullable=False)
+    PhoneNumber = db.Column(db.String(12), unique=True, nullable=True)
+    Email = db.Column(db.String(320), unique=True, nullable=True)
+
+
+class Messages(db.Model) :
+    MessageID = db.Column(db.integer, primary_key=True)
+    FirstName = db.Column(db.String(25), unique=False, nullable=False)
+    LastName = db.Column(db.String(25), unique=False, nullable=False)
+    PhoneNumber = db.Column(db.String(12), unique=True, nullable=True)
+    Email = db.Column(db.String(320), unique=True, nullable=True)
+    Comment = db.Column(db.String(1000), unique=False, nullable=False)
+
+
+class Listings(db.Model) :
+    PropertyID = db.Column(db.integer, primary_key=True)
+    Location = db.Column(db.String(255), unique=False, nullable=False)
+    City = db.Column(db.String(255), unique=False, nullable=False)
+    State = db.Column(db.String(2), unique=False, nullable=False)
+    Zip = db.Column(db.String(5), unique=False, nullable=False)
+    Price = db.Column(db.Float(15, 2), unique=False, nullable=False)
+    Bedroom = db.Column(db.integer, unique=False, nullable=False)
+    Bathroom = db.Column(db.integer, unique=False, nullable=False)
+    SquareFeet = db.Column(db.Float(15, 2), unique=False, nullable=False)
+    Description = db.Column(db.String(1000), unique=False, nullable=False)
+
+
+class User(db.Model) :
+    UserID = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(255), unique=True, nullable=False)
+
+# Use only first time to create otherwise comment out! 
+# If not you may have to use cmd prompt to make db instead.
+
+#db.create_all()
+
+# Creating ModelView for each table for use with Flask-Admin
+admin.add_view(ModelView(Clients, db.session))
+admin.add_view(ModelView(Messages, db.session))
+admin.add_view(ModelView(Listings, db.session))
+    
+if __name__ == '__main__':
+    # Should be able to access admin from localhost/admin in url
+     app.run(debug=True)
